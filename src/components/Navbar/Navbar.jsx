1a821aa4fd1935081554e2cb6be4972bed3fc061
arifbiswas <arifbiswas1020@gmail.com>
2023-05-09 12:15:25 +0600

    start demo project

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Navbar = () => {
+    return (
+        <div className='bg-slate-500 h-32 w-full max-w-[1400] m-auto'>
+            <h1 className='text-3xl font-semibold'>Travel bd</h1>
+        </div>
+    );
+};
+
+export default Navbar;
